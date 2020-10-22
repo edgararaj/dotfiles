3f49d6ce9c5b28146faa6f1b59c1667882992da4
Edgar Araújo <edgararaj@gmail.com>
2020-10-22 21:53:29 +0100

    Arroz primo

--- fetch-files.sh
+++ fetch-files.sh
@@ -2,6 +2,7 @@
 
 mkdir -p .config/nvim/
 mkdir -p .config/Code\ -\ OSS/User/
+cp -r ~/bin .
 cp -r ~/.config/nvim/* .config/nvim/
 cp -r ~/.config/Code\ -\ OSS/User/settings.json .config/Code\ -\ OSS/User/
 cp ~/.bashrc .
